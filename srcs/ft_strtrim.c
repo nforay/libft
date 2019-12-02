da34ceeb8efead9f06af724a124d7e8f15795bc0
nforay <nforay@student.42.fr>
2019-12-02 22:43:44 +0100

    optimisation strtrim

--- srcs/ft_strtrim.c
+++ srcs/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: nforay <nforay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/01 23:34:30 by nforay            #+#    #+#             */
-/*   Updated: 2019/12/02 22:37:50 by nforay           ###   ########.fr       */
+/*   Updated: 2019/12/02 22:43:05 by nforay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 char 		*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
-	size_t	j;
 	size_t	len;
-	char	*ptr;
 
 	i = 0;
 	if (!s1 || !set)
@@ -27,14 +25,5 @@ char 		*ft_strtrim(char const *s1, char const *set)
 	len = ft_strlen(&s1[i]);
 	while (len && ft_strchr(set, s1[(i + (len - 1))]))
 		len--;
-	if (!(ptr = (char*)malloc(len + 1)))
-		return (NULL);
-	j = 0;
-	while (len--)
-		{
-			ptr[j] = s1[i + j];
-			j++;
-		}
-	ptr[j] = '\0';
-	return (ptr);
+	return (ft_substr(s1, i, len));
 }
