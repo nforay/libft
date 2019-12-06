ddc693ceff5b23803d924187e591eeef564981bd
nforay <nforay@student.42.fr>
2019-12-06 02:49:19 +0100

    Added ft_lstclear

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: nforay <nforay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 14:08:26 by nforay            #+#    #+#             */
-/*   Updated: 2019/12/06 02:01:26 by nforay           ###   ########.fr       */
+/*   Updated: 2019/12/06 02:20:41 by nforay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,4 +63,5 @@ int					ft_lstsize(t_list *lst);
 t_list 				*ft_lstlast(t_list *lst);
 void 				ft_lstadd_back(t_list **alst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void*));
+void				ft_lstclear(t_list **lst, void (*del)(void*));
 #endif
