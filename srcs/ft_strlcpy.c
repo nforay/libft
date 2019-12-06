e3252a21fc50a665ec8932e32e3be5685bf5fcb2
nforay <nforay@student.42.fr>
2019-12-06 16:41:38 +0100

    Added protection

--- srcs/ft_strlcpy.c
+++ srcs/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: nforay <nforay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 10:44:33 by nforay            #+#    #+#             */
-/*   Updated: 2019/12/01 18:43:27 by nforay           ###   ########.fr       */
+/*   Updated: 2019/12/06 16:40:46 by nforay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@ size_t	ft_strlcpy(char *dest, char *src, size_t size)
 	size_t	i;
 
 	i = 0;
-	if (size == 0)
+	if (!src)
+		return (0);
+	if (!size)
 		return (ft_strlen(src));
 	while (i < (size - 1) && src[i] != '\0')
 	{
-		if ((dest[i] = src[i]) == '\0')
-			break;
+		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
